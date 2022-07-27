0e7b52a299044bb7cea11edffabc8b81b87a8f41
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-07-27 10:48:50 +0100

    not replicating messages of the sender

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,10 @@ use tokio::{
 async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
 
-    let (tx, _rx) = broadcast::channel::<String>(10);
+    let (tx, _rx) = broadcast::channel(10);
 
     loop {
-        let (mut socket, _addr) = listener.accept().await.unwrap();
+        let (mut socket, addr) = listener.accept().await.unwrap();
 
         let tx = tx.clone();
         let mut rx = tx.subscribe();
@@ -28,12 +28,15 @@ async fn main() {
                         if result.unwrap() == 0 {
                             break;
                         }
-                        tx.send(line.clone()).unwrap();
+                        tx.send((line.clone(), addr)).unwrap();
                         line.clear();
                     }
                     result = rx.recv() => {
-                        let msg = result.unwrap();
-                        writer.write_all(msg.as_bytes()).await.unwrap();
+                        let (msg, other_addr) = result.unwrap();
+
+                        if addr != other_addr {
+                            writer.write_all(msg.as_bytes()).await.unwrap();
+                        }
                     }
                 }
             }
