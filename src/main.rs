2ec21452a037817eb0b4b1088b78aacb8e9cd353
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-07-27 10:22:27 +0100

    using tokio BufReader

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt},
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
     net::TcpListener,
 };
 
@@ -9,9 +9,18 @@ async fn main() {
 
     let (mut socket, _addr) = listener.accept().await.unwrap();
 
-    let mut buffer = [0u8; 1024];
+    let (reader, mut writer) = socket.split();
 
-    let bytes_read = socket.read(&mut buffer).await.unwrap();
+    let mut reader = BufReader::new(reader);
+    let mut line = String::new();
 
-    socket.write_all(&buffer[..bytes_read]).await.unwrap();
+    loop {
+        let bytes_read = reader.read_line(&mut line).await.unwrap();
+        if bytes_read == 0 {
+            break;
+        }
+
+        writer.write_all(&line.as_bytes()).await.unwrap();
+        line.clear();
+    }
 }
